eac40d00177b6d2ab3ffa857a9528bd17ddffbc8
Marc Tucker <marcttucker@gmail.com>
2020-09-17 16:22:14 -0400

    updated migration code

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config');
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
 
@@ -15,11 +16,13 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+app.use('/api/articles', articlesRouter);
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 })
 
-+ app.use(function errorHandler(error, req, res, next) {
+app.use(function errorHandler(error, req, res, next) {
   let response
   if(NODE_ENV === 'production') {
     response = { error: { message: 'server error' } }
